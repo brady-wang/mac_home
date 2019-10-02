c56a739c31d3c0f62d26d8512fe1a90c036a1f96
brady <brady.wang@qq.com>
2019-10-02 19:47:27 +0800

    t

---
+++ vue/test/src/TodoList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+            <input type="text" v-model="inputValue">
+            <button @click="handClick">提交 </button>
+
+            <ul>
+                <todo-item 
+                v-for="(item ,index) in list"
+                 :key="index"
+                 :content="item"
+                 :index="index"
+                 @delete="handDelete"
+                 >{{item}}</todo-item>
+            </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem"
+export default {
+    components:{
+        'todo-item':TodoItem
+    },
+    data () {
+        return {
+            inputValue: '',
+            list:[]
+        }
+    },
+    methods:{
+        handClick(){
+            this.list.push(this.inputValue)
+            this.inputValue = ''
+        },
+        handDelete(index){
+            this.list.splice(index,1)
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ vue/test/src/components/TodoItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li @click="handClick"> {{content}}</li>
+</template>
+
+<script>
+export default {
+    props:['content','index'],
+    methods:{
+        handClick(){
+            this.$emit('delete',this.index)
+        }
+    }
+}
+</script>
